929d560e7732bbf74fcfd918f5e73c4ab21ac797
Kaio-code-creator <kaioasousa12@gmail.com>
2024-06-17 16:39:13 -0300

    Add files via upload

---
+++ JS/Estudo/Repetições-JS/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do {
+    console.log(`Cor ${c}`)
+    c++
+}while (c <= 1)
+
+var c = 1
+while (c <= 6) {
+    console.log(`Traço ${c}`)
+    c++
+}
+
+for (var c=1;c<=10;c++) {
+    console.log(`Passo ${c}`)
+}
+
+console.log('Vai começar...')
+for(var c =1; c <= 4;c++) {
+    console.log(c)
+}
+console.log('FIM')
---
+++ JS/Estudo/condições/ex4.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var diaS = agora.getDay()
+switch(diaS) {
+    case 0:
+        console.log('domingo')
+        break
+    case 1:
+        console.log('segunda')
+        break
+    case 2:
+        console.log('terça')
+        break
+    case 3:
+        console.log('quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+     case 6:
+        console.log('sábado')
+        break
+    default:
+            console.log('[ERRO] Dia inválido')
+            break
+}
