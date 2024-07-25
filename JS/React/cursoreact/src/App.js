1dcb02ec6e9979a92d0d8e72aa163a43511c25b5
Kaio-code-creator <kaioasousa12@gmail.com>
2024-07-25 11:41:19 -0300

    .

--- JS/React/cursoreact/src/App.js
+++ JS/React/cursoreact/src/App.js
@@ -11,16 +11,37 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(title);
+
+    setTitle("");
+    
+    console.log("Enviou");
+  };
+
   return (
     <div className="App">
       <div className="todo-header">
         <h1>React Todo</h1>
       </div>
       <div className="form-todo">
-        <p>Formulário</p>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onsubmit={handleSubmit}></form>
+          <div className="form-control">
+            <label htmlFor="title">O que você vai fazer?</label>
+            <input type="text" name="title" placeholder="Título da tarefa" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title || ""}
+            required
+            />
+          </div>
+          <input type="submit" value="Enviar" />
       </div>
       <div className="list-todo">
-        <p>Lista</p>
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas</p>}
       </div>
     </div>
   );
